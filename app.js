82d5149349f8645d51100bf64c95661b11e34893
Jason <bjason@gmail.com>
2014-11-08 21:29:44 -0800

    http example

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
+var http = require("https");
+
 var express = require('express');
+
+
 
 var sampleModule = require("./sample");
 
@@ -11,4 +15,26 @@ app.get('/', function(req, res){
       res.send('hello world ' + answer);
 });
 
+
+var options = {
+  host: "https://rest.sqor.com",
+  port: 80,
+  path: '/resource?id=foo&bar=baz',
+  method: 'POST'
+};
+
+http.request(options, function(res) {
+	debugger;
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+  res.setEncoding('utf8');
+  res.on('data', function (chunk) {
+    console.log('BODY: ' + chunk);
+  });
+}, function(eee){ console.log(eee);}).end();
+
+console.log('wtf');
+
 app.listen(3000);
+
+

f0cc79488cc9288da8b7fb6d0520397dd421309f
Jason <bjason@gmail.com>
2014-11-10 02:59:10 -0800

    Wow, a request finally worked....

--- rest_example.js
+++ rest_example.js
@@ -1,5 +1,8 @@
 var request = require('request');
-request('http://www.google.com', function (error, response, body) {
+request('https://sqor.com/rest/entities/26739?detail=full', function (error, response, body) {
+	console.log(response);
+	console.log(body);
+	console.log(error);
   if (!error && response.statusCode == 200) {
     console.log(body) // Print the google web page.
   }
